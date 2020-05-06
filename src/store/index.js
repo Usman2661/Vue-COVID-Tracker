9eb33dbf96f4bbf325f0805341b5721728328a4b
Usman Ali <uali@modern-networks.co.uk>
2020-05-06 14:53:13 +0100

    added vuex store

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import covid from './modules/covid';
+
+//Load Vuex
+Vue.use(Vuex);
+
+//Create Store
+
+export default new Vuex.Store({
+  modules: {
+    covid,
+  },
+});
---
+++ src/store/modules/covid.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const state = {
+  summary: {},
+};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
